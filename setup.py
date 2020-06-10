0f142a5e77bf1cfe85d9578d2b45f27a9f72f096
eokim <michael.phan127@gmail.com>
2020-06-10 02:19:37 +0000

    add required python version

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import find_packages, setup
 
-with open('READ.md', 'r') as f:
+with open('README.md', 'r') as f:
 	long_description = f.read()
 
 setup(
 	name='pgbackup',
 	version='0.1.0',
-	author='Michael Phan'
-	authur_email='test@testmail.com'
+	author='Michael Phan',
+	author_email='test@testmail.com',
 	description='A utility for backuping up PostgreSQL databases.',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	url='https://github.com/eokim/testpgb',
-	packages=find_packages('src')
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	install_requires=['boto3'],
+	python_requires='>3.6',
+	entry_points={
+		'console_scripts': [
+			'pgbackup=pgbackup.cli:main'
+			],
+		}
 )
